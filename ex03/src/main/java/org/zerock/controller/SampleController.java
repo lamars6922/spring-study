0ce2f477665bc907930e67eee31796cadedaeb86
나화성 <@DESKTOP-CB0M8OI>
2021-09-30 23:08:03 +0900

    chapter 16

--- ex03/src/main/java/org/zerock/controller/SampleController.java
+++ ex03/src/main/java/org/zerock/controller/SampleController.java
@@ -6,11 +6,17 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.domain.SampleVO;
+import org.zerock.domain.Ticket;
 
 import lombok.extern.log4j.Log4j;
 
@@ -49,5 +55,32 @@ public class SampleController {
 		Map<String, SampleVO> map = new HashMap<>();
 		map.put("First",  new SampleVO(111, "그루트", "주니어"));
 		return map;
+	}
+	// 데이터와 함께 HTTP 헤더의 상태 메시지 등을 같이 전달
+	@GetMapping(value="/check", params = {"height", "weight"})
+	public ResponseEntity<SampleVO> check(Double height, Double weight) {
+		SampleVO vo = new SampleVO(0, "" + height, ""+ weight);
+		ResponseEntity<SampleVO> result = null;
+		if(height<150) {
+			result = ResponseEntity.status(HttpStatus.BAD_GATEWAY).body(vo);
+		} else {
+			result = ResponseEntity.status(HttpStatus.ACCEPTED.OK).body(vo);
+		}
+		
+		return result;
+	}
+	
+	@GetMapping("/product/{cat}/{pid}")
+	public String[] getPath(
+				@PathVariable("cat") String cat,
+				@PathVariable("pid") Integer pid
+			) {
+		return new String[] {"category : " + cat, "productid : " + pid};
+	}
+	
+	@PostMapping("/ticket")
+	public Ticket convert(@RequestBody Ticket ticket) {
+		log.info("convert.........ticket" + ticket);
+		return ticket;
 	}
 }
