dab6cf28b9e6f5895c5bee262cabe4c4cc6ca17f
나화성 <@DESKTOP-CB0M8OI>
2021-11-17 22:59:52 +0900

    18 AOP라는 패러다임

--- ex04/src/test/java/org/zerock/service/SampleServiceTests.java
+++ ex04/src/test/java/org/zerock/service/SampleServiceTests.java
@@ -22,4 +22,15 @@ public class SampleServiceTests {
 		log.info(service);
 		log.info(service.getClass().getName());
 	}
+	
+	@Test
+	public void testAdd() throws Exception {
+		log.info(service.doAdd("123", "456"));
+	}
+/*	
+	@Test
+	public void testAddErr() throws Exception {
+		log.info(service.doAdd("123",  "ABC"));
+	}
+	*/
 }
