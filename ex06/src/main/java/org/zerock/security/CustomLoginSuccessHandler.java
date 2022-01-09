401e0861f2d0b2e9cd75476c6ed7bb05aae3515e
나화성 <@DESKTOP-CB0M8OI>
2022-01-09 23:23:22 +0900

    JDBC를 이용하는 간편 인증/권한 처리

--- ex06/src/main/java/org/zerock/security/CustomLoginSuccessHandler.java
+++ ex06/src/main/java/org/zerock/security/CustomLoginSuccessHandler.java
@@ -43,7 +43,7 @@ public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
 			response.sendRedirect("/sample/member");
 			return;
 		}
-
+		
 		response.sendRedirect("/");
 	}
 }
